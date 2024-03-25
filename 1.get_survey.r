41cada05e2cd8a957ac05ab4720becd701890552
Sith Jaisong <126034275+kanthjs@users.noreply.github.com>
2024-03-25 22:05:49 +0700

    add station name
    add station name in get_survey.r

--- 1.get_survey.r
+++ 1.get_survey.r
@@ -1,13 +1,22 @@
-data = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTfroOxkmc-3ZYKBQ2LerRw2tmK2C3aVMJiIXbpta7I48U42qgeUosPh2dmSYkF1EAid7iUKJflylEL/pub?gid=769414447&single=true&output=csv"
-
+#
+#
+#
+# load package #
 library(tidyverse)
+library(vroom)
+survel_url <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vTfroOxkmc-3ZYKBQ2LerRw2tmK2C3aVMJiIXbpta7I48U42qgeUosPh2dmSYkF1EAid7iUKJflylEL/pub?gid=769414447&single=true&output=csv"
 
-survey = read_csv(file = data, col_types = "ccfcfddfffdddddddddddddddddddddddddddddcc")
+survey = read_csv(file = survey_url, 
+                  col_types = "ccfcfddfffdddddddddddddddddddddddddddddcc")
+# ignore warning message
 
-rdstationE_T = read_csv()
+# data frame for replace thai data with eng data
+station_name_url <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vSajGerDBPsitZjCEOYhv2ls704QBXkWP9haY3BpARHCtrOTjWZVJyRgrf2BrKlSZPCr-Px-VAm__Qe/pub?gid=0&single=true&output=csv" 
 
-provinceE_T = read_csv()
+rdstationE_T = read_csv(file = station_name_url, col_types = "ff")
 
-RiceStageE_T = read_csv()
+#provinceE_T = read_csv()
 
-RiceVarietyE_T = read_csv()
+#RiceStageE_T = read_csv()
+
+#RiceVarietyE_T = read_csv()
